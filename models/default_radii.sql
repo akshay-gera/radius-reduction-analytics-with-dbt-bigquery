0d9fe50fa5237fa4cb2fd68a964144d45494f43c
Akshay Gera <98545133+akshay-gera@users.noreply.github.com>
2025-01-08 09:03:55 +0100

    Default Radii periods changed

--- models/default_radii.sql
+++ models/default_radii.sql
@@ -1,10 +1,11 @@
 WITH default_radii AS(
-    SELECT
-        delivery_area_id,
-        time_from AS event_started_timestamp,
-        time_to AS event_ended_timestamp,
-        current_radius AS default_radius
-    FROM {{ ref('radius_change_history') }}
-
+  SELECT 
+    delivery_area_id,
+    time_from AS event_started_timestamp,
+    LEAD(time_from) OVER(PARTITION BY delivery_area_id ORDER BY time_from) AS event_ended_timstamp,
+    default_radius_value AS default_radius
+  FROM {{ ref('radius_change_history') }}
+  WHERE is_temporary_change='No'
+  ORDER BY delivery_area_id,time_from
 )
-SELECT * FROM default_radii
+SELECT * FROM default_radii 
