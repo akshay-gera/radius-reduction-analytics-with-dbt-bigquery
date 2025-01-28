ae74b22a72fb86c23067bafdfac12428309b3227
Akshay Gera <98545133+akshay-gera@users.noreply.github.com>
2025-01-28 11:50:46 +0100

    Changed model definition of radius reductions by joining default_radii table

--- models/radius_reductions.sql
+++ models/radius_reductions.sql
@@ -1,16 +1,20 @@
-WITH radius_reductions AS(
-    SELECT
-    r.delivery_area_id,
-    r.time_from,
-    r.time_to,
-    d.default_radius,
-    r.current_radius,
-    r.radius_change,
-    r.duration
-    
-    FROM {{ ref('radius_change_history') }} AS r
-    LEFT JOIN {{ ref('default_radii') }} AS d
-    ON r.delivery_area_id = d.delivery_area_id AND time_from >= event_started_timestamp AND time_to <= event_ended_timstamp 
-    WHERE current_radius <> default_radius
+WITH radius_comparison AS(
+  SELECT 
+      r.delivery_area_id,
+      time_from,
+      time_to,
+      current_radius,
+      dr.default_radius, 
+      CASE 
+        WHEN current_radius - dr.default_radius < 0 THEN 'reduced'
+        WHEN current_radius - dr.default_radius = 0 THEN 'no change'
+        ELSE 'increased' 
+      END  AS change
+  FROM {{ref('radius_change_history')}} AS r
+  LEFT JOIN {{ ref('default_radii') }} dr
+    ON r.delivery_area_id = dr.delivery_area_id AND time_from >= event_started_timestamp AND time_from< event_ended_timstamp
+  ORDER BY r.delivery_area_id, time_from
+
 )
-SELECT * FROM radius_reductions
+SELECT * FROM radius_comparison
+WHERE change = 'reduced'  
